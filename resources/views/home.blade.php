8c48f0f331c570967c12ed50dd7f56ee1474fcf5
Nova Eliza Maharani <novaelzrayya@gmail.com>
2025-02-17 19:27:01 +0700

    Home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Home</title>
+    </head>
+    <body>
+        <h2>Selamat Datang di Website Nova's Store</h2>
+        <h>Kami menyediakan berbagai macam kebutuhan Anda dengan harga terjangkau ^-^</h3>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+
