257917467d92895a67a9668f87443817c65a9c4b
Nova Eliza Maharani <novaelzrayya@gmail.com>
2025-02-18 20:03:52 +0700

    Sales

---
+++ resources/views/sales/index.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Sales</title>
+    </head>
+    <body>
+        <h1>Halaman Penjualan</h1>
+        <p>Silakan lakukan transaksi penjualan di sini.</p>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\SalesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,3 +31,5 @@ Route::prefix('/category')->group(function () {
 });
 
 Route::get('/user/{id}/name/{name}', [UserController::class, 'profile'])->name('user.profile');
+
+Route::get('/sales', [SalesController::class, 'index'])->name('sales');
